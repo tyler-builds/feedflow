bbf6a50416689d42d549dfff015a2d2f5f6020d3
Tyler Carr <tylercarr1111@yahoo.com>
2025-11-11 22:27:07 -0600

    Add production settings to sentry config

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -5,11 +5,11 @@ Sentry.init({
 
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
-  sendDefaultPii: true,
+  sendDefaultPii: import.meta.env.PROD ? false : true,
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for tracing.
   // We recommend adjusting this value in production.
   // Learn more at https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
-  tracesSampleRate: 1.0,
+  tracesSampleRate: import.meta.env.PROD ? 0.1 : 1.0,
 });
--- src/router.tsx
+++ src/router.tsx
@@ -48,7 +48,7 @@ export function getRouter() {
 
       // Adds request headers and IP for users, for more info visit:
       // https://docs.sentry.io/platforms/javascript/guides/tanstackstart-react/configuration/options/#sendDefaultPii
-      sendDefaultPii: true,
+      sendDefaultPii: import.meta.env.PROD ? false : true,
 
       integrations: [Sentry.tanstackRouterBrowserTracingIntegration(router)],
 
@@ -56,7 +56,7 @@ export function getRouter() {
       // of transactions for tracing.
       // We recommend adjusting this value in production.
       // Learn more at https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
-      tracesSampleRate: 1.0,
+      tracesSampleRate: import.meta.env.PROD ? 0.1 : 1.0,
     });
   }
 
